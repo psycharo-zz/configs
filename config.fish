e84b7699b68d40aa392d1f6605f97a6b79b06614
Timur Bagautdinov <timur.bagautdinov@neuralconcept.com>
2019-02-05 11:11:46 +0000

    config update

--- config.fish
+++ config.fish
@@ -1,5 +1,13 @@
 set fish_greeting ""
 
+if not functions -q fundle; eval (curl -sfL https://git.io/fundle-install); end
+fundle plugin 'edc/bass'
+fundle plugin 'tuvistavie/fish-kubectl'
+fundle plugin 'tuvistavie/fish-ssh-agent'
+fundle plugin 'tuvistavie/fish-fastdir'
+fundle plugin 'brgmnn/fish-docker-compose'
+fundle init
+
 set -x TERM "xterm-256color"
 
 alias e "emacsclient -t"
